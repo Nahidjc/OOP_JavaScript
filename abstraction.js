9f768db7789f2590c22e3b22bc83f0b306b0f9f1
Nahidjc <nahidjc80@gmail.com>
2021-06-20 20:13:57 +0600

    abstraction

---
+++ abstraction.js
@@ -0,0 +1,24 @@
+class Animal {
+
+    talk() {
+        throw new Error("You must implement talk method")
+    }
+}
+
+class Cat extends Animal {
+
+    talk() {
+        console.log('meow meow');
+    }
+}
+class Dog extends Animal {
+
+    talk() {
+        console.log('bark bark');
+    }
+}
+
+const myCat = new Cat();
+myCat.talk();
+const myDog = new Dog();
+myDog.talk();
